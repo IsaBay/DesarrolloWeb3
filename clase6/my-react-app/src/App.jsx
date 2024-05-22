d2d6620914321827838a77961f16b8d3da5ec075
IsaBay <isabellabaylac@gmail.com>
2024-05-22 17:00:49 -0300

    Trabajo realizado

---
+++ clase6/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import UserList from './UserList'
+
+function App() {
+  
+
+  return (
+  
+    <div>
+    <h1>App React con MockAPI.io</h1>
+    <UserList />
+    </div>
+
+  )
+}
+
+export default App
