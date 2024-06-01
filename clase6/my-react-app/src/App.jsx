a9eb9a41eecb1f1496b6e1faa77a1ff2b5924d25
IsaBay <isabellabaylac@gmail.com>
2024-06-01 11:56:29 -0300

    clase-7

--- clase6/my-react-app/src/App.jsx
+++ clase6/my-react-app/src/App.jsx
@@ -1,4 +1,8 @@
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
 import UserList from './UserList'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   
@@ -7,7 +11,27 @@ function App() {
   
     <div>
     <h1>App React con MockAPI.io</h1>
-    <UserList />
+    {/* <UserList /> */}
+    <Router>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/users">Users</Link></li>
+      </ul>
+    </nav>
+
+
+
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/about" element={<About />} />
+<Route path="/contact" element={<Contact />} />
+<Route path="/users" element={<UserList />} />
+</Routes>
+</Router>
+
     </div>
 
   )
